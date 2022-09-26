06c272ddc443c5909989720ff823c463cdfdb14e
SilverSscholar <silversscholar@gmail.com>
2022-09-26 07:56:28 -0400

    init

--- solution.py
+++ solution.py
@@ -8,7 +8,12 @@ root.title('Currency-Conversion')
 root.iconbitmap('c:/gui/codemy.ico')
 root.geometry("500X500")
 
+#Create Tabs
+my_notebook = ttk.Notebook(root)
+my_notebook.pack(pady=5)
 
 
-
+#Create Two Frames
+currency_frame = Frame(my_notebook)
+conversion_frame
 root.mainloop()
