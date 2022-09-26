aa9dbf08decb9ebe77d1bf3e5c8540a8ec409c36
SilverSscholar <silversscholar@gmail.com>
2022-09-26 07:54:11 -0400

    Commit_01

---
+++ solution.py
@@ -0,0 +1,14 @@
+from tkinter import*
+from tkinter import ttk
+from tkinter import messagebox
+
+
+root =Tk()
+root.title('Currency-Conversion')
+root.iconbitmap('c:/gui/codemy.ico')
+root.geometry("500X500")
+
+
+
+
+root.mainloop()
